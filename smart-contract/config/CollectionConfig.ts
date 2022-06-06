aac11f40ab435f151177c3b192865e22e4ad50f1
SpaceFreakz <spacefreakznft@gmail.com>
2022-06-06 11:52:27 -0300

    upup

---
+++ smart-contract/config/CollectionConfig.ts
@@ -0,0 +1,35 @@
+import CollectionConfigInterface from '../lib/CollectionConfigInterface';
+import { ethereumTestnet, ethereumMainnet } from '../lib/Networks';
+import { openSea } from '../lib/Marketplaces';
+import whitelistAddresses from './whitelist.json';
+
+const CollectionConfig: CollectionConfigInterface = {
+  testnet: ethereumTestnet,
+  mainnet: ethereumMainnet,
+  // The contract name can be updated using the following command:
+  // yarn rename-contract NEW_CONTRACT_NAME
+  // Please DO NOT change it manually!
+  contractName: 'SpaceTests',
+  tokenName: 'Space Tests SPESTS',
+  tokenSymbol: 'SPESTS',
+  hiddenMetadataUri: 'ipfs://QmRoB3hbM9r1KaRQ9x2RyoKaeAAZyrUQGSSvQJ1PvigwPR/hidden.json',
+  maxSupply: 20,
+  whitelistSale: {
+    price: 0.05,
+    maxMintAmountPerTx: 1,
+  },
+  preSale: {
+    price: 0.07,
+    maxMintAmountPerTx: 2,
+  },
+  publicSale: {
+    price: 0.09,
+    maxMintAmountPerTx: 5,
+  },
+  contractAddress: "0xfA4D8fA05515449C7473f8A0dd18E1AF86a006A4",
+  marketplaceIdentifier: 'my-nft-token',
+  marketplaceConfig: openSea,
+  whitelistAddresses: whitelistAddresses,
+};
+
+export default CollectionConfig;
