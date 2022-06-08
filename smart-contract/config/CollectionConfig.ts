838719d5cf551b72e392403c23d095289416c8e4
ends87 <endsavlis@gmail.com>
2022-06-08 13:56:31 -0400

    Whitelist-update

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -4,15 +4,15 @@ import * as Marketplaces from '../lib/Marketplaces';
 import whitelistAddresses from './whitelist.json';
 
 const CollectionConfig: CollectionConfigInterface = {
-  testnet: Networks.ethereumTestnet,
-  mainnet: Networks.ethereumMainnet,
+  testnet: Networks.polygonTestnet,
+  mainnet: Networks.polygonMainnet,
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
   contractName: 'SpaceTests',
   tokenName: 'My NFT Token',
   tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
+  hiddenMetadataUri: 'ipfs://QmRoB3hbM9r1KaRQ9x2RyoKaeAAZyrUQGSSvQJ1PvigwPR/hidden.json',
   maxSupply: 10000,
   whitelistSale: {
     price: 0.05,
@@ -26,8 +26,8 @@ const CollectionConfig: CollectionConfigInterface = {
     price: 0.09,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  contractAddress: '0x049A196Df77D8430ea8Dc465c01C768471Bcb083',
+  marketplaceIdentifier: 'my-nft-tokenSPTS',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
